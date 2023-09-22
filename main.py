8ae4bb870e19d73e30ae65e6b89de38c3695ceda
Priyanshu Sarangi <sarangipriyanshu@gmail.com>
2023-09-22 09:59:34 +0530

    Backend

---
+++ main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI,HTTPException,UploadFile,File
+import io
+from io import BytesIO
+from ultralytics import YOLO
+import pandas as pd
+import os
+import numpy as np
+import cv2
+from fastapi.responses import RedirectResponse,StreamingResponse
+import json
+ 
+
+from Helper.helperfunctions import add_BoundingBoxes,get_Images_from_Bytes,get_bytes_from_Images,get_model_predict,save_image
+
+
+
+app=FastAPI(title="Get To Know ur Exercise")
+
+@app.on_event("startup")
+def startupevent():
+    if not os.path.exists("Prediction"):
+        os.makedirs("Prediction")
+
+    # if not os.path.exists("tmp"):
+    #     os.makedirs("tmp")
+    print("Start Done") 
+
+
+
+@app.get('/')
+async def redirect():
+    return RedirectResponse("/docs")
+
+
+
+@app.post("/predict_to_know_the_exericise")
+async def predict_to_json(file: UploadFile = File(...)):
+    model = YOLO("Big DAta.pt")
+    image = get_Images_from_Bytes(file.file.read())
+    predictions = get_model_predict(image, model)
+    # print(predictions[0].boxes.boxes)
+    # print("______")
+    # print(predictions[0].boxes)
+    # print("______")
+
+    # print(predictions[0]) 
+    # print("______")
+
+    # print(predictions)
+    # print(predictions[0].boxes.data[0][-1])
+    # data = str(predictions)
+     
+    info= {
+        "Exercise_type":str(predictions[0,5].item())
+    }
+    return info
+   
+@app.post("/predict_save_image")
+async def predict_and_save(file:UploadFile=File(...)):
+    model=YOLO("Big DAta.pt")
+    img=get_Images_from_Bytes(file.file.read())
+    predictions=get_model_predict(img,model)
+    bb_box=add_BoundingBoxes(img,predictions)
+    processed_image_bytes=get_bytes_from_Images(bb_box)
+    save_image(file_name=file.filename,image=bb_box,prediction=predictions)
+    return StreamingResponse(content=processed_image_bytes,media_type="image/jpeg") 
+
